1b310d5d3f49cffe45e67116592ccd75d39c1ab1
BookCloud <some0choo@gmail.com>
2020-07-30 07:10:33 +0800

    antd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,46 @@
-import styles from './index.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+import './index.css';
 
 export default function() {
+  const { Header, Content, Footer } = Layout;
   return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>Hello Worlds</li>
-      </ul>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content">Content</div>
+        <div>
+          <h1>GoodBye World</h1>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+
+          <hr />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Professional Design {new Date().getFullYear()} Created by Professional Choo
+      </Footer>
+    </Layout>
   );
 }
